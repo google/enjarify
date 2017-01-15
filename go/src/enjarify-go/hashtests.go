0cb0cae2d3b5fcac1f62a0f7d943c61edd456005
Robert Grosse <n210241048576@gmail.com>
2017-01-15 10:11:50 -0800

    channel passing

--- go/src/enjarify-go/hashtests.go
+++ go/src/enjarify-go/hashtests.go
@@ -26,10 +26,14 @@ func hash(s string) string {
 	return string(digest[:])
 }
 
+type OutputChan chan [][2]string
+
 func hashTests() {
 	// defer profile.Start().Stop()
-	sem := make(chan struct{}, 9999)
-	outputs := [256 * 7][][2]string{}
+	outputChan := make(OutputChan)
+	syncChan := make(chan OutputChan, 1)
+	syncChan2 := make(chan OutputChan, 1)
+	syncChan <- outputChan
 
 	for test := 1; test < 8; test++ {
 		name := fmt.Sprintf("test%d", test)
@@ -37,26 +41,26 @@ func hashTests() {
 		rawdex := Read(path.Join(dir, "classes.dex"))
 
 		for bits := 0; bits < 256; bits++ {
-			go func(test, bits int, rawdex string) {
+			go func(bits int, rawdex string, inchan <-chan OutputChan, outchan chan<- OutputChan) {
 				opts := jvm.Options{bits&1 == 1, bits&2 == 2, bits&4 == 4, bits&8 == 8, bits&16 == 16, bits&32 == 32, bits&64 == 64, bits&128 == 128}
 				results := translate(opts, rawdex)
 
 				output := make([][2]string, len(results))
-				outputs[(test-1)*256+bits] = output
-
 				for i := range results {
 					cls := results[i][1]
 					util.Assert(cls != "")
 					output[i][0] = cls
 					output[i][1] = hash(cls)
 				}
-				sem <- struct{}{}
-			}(test, bits, rawdex)
-		}
-	}
 
-	for i := 0; i < 7*256; i++ {
-		<-sem
+				c := <-inchan
+				c <- output
+				outchan <- c
+			}(bits, rawdex, syncChan, syncChan2)
+
+			syncChan = syncChan2
+			syncChan2 = make(chan OutputChan, 1)
+		}
 	}
 
 	fullhash := ""
@@ -64,7 +68,8 @@ func hashTests() {
 		name := fmt.Sprintf("test%d", test)
 		fmt.Print(name + "\n")
 		for bits := 0; bits < 256; bits++ {
-			for _, pair := range outputs[(test-1)*256+bits] {
+			output := <-outputChan
+			for _, pair := range output {
 				fmt.Printf("%08b %x\n", uint8(bits), pair[1])
 				fullhash = hash(fullhash + pair[0])
 			}
